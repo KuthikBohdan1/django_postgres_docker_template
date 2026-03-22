607cd16142f08fbf9751c72391affdb26ec65627
bogdan <bkuzik14@gmail.com>
2026-03-22 20:10:36 +0200

    hot fix db

--- project_name/settings.py
+++ project_name/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'postgres',
-        "USER": os.getenv("POSTGRES_USER"),
-        "HOST": 'pdb', ##container name
-        "PORT": 5432,
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": 'postgres',
+#       "USER": os.getenv("POSTGRES_USER"),
+#        "HOST": 'pdb', ##container name
+#        "PORT": 5432,
+#       "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+#    }
+#
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
