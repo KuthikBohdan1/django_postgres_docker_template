9d0685bbdf69d64a3c6c4adefd1d94d79754f9ef
bogdan <bkuzik14@gmail.com>
2026-03-22 19:37:28 +0200

    hotfix project name

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project project.
+WSGI config for project_name project_name.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_name.settings')
 
 application = get_wsgi_application()
